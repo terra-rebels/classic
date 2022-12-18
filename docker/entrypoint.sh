fc8d53bb1226cb40658c93d882ae41b184db016a
Justin Tabish <jtabish@darksystems.ca>
2022-12-18 12:28:00 -0800

    updated docker files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,32 +6,30 @@ MONIKER="${MONIKER:-docker-node}"
 ENABLE_LCD="${ENABLE_LCD:-true}"
 MINIMUM_GAS_PRICES=${MINIMUM_GAS_PRICES-0.01133uluna,0.15uusd,0.104938usdr,169.77ukrw,428.571umnt,0.125ueur,0.98ucny,16.37ujpy,0.11ugbp,10.88uinr,0.19ucad,0.14uchf,0.19uaud,0.2usgd,4.62uthb,1.25usek,1.25unok,0.9udkk,2180.0uidr,7.6uphp,1.17uhkd}
 SNAPSHOT_NAME="${SNAPSHOT_NAME}"
-SNAPSHOT_BASE_URL="${SNAPSHOT_BASE_URL:-https://getsfo.quicksync.io}"
+SNAPSHOT_BASE_URL="${SNAPSHOT_BASE_URL:-https://dl2.quicksync.io}"
 ENABLE_UNSAFE_CORS="${VALIDATOR_ENABLE_UNSAFE_CORS:-false}"
 CORS_ALLOWED_ORIGINS="${CORS_ALLOWED_ORIGINS:-[]}"
 CORS_ALLOWED_METHODS="${CORS_ALLOWED_METHODS:-["HEAD", "GET", "POST"]}"
 CORS_ALLOWED_HEADERS="${CORS_ALLOWED_HEADERS:-["Origin", "Accept", "Content-Type", "X-Requested-With", "X-Server-Time"]}"
 
-# First sed gets the app.toml moved into place.
-# app.toml updates
-sed 's/minimum-gas-prices = "0uluna"/minimum-gas-prices = "'"$MINIMUM_GAS_PRICES"'"/g' ~/app.toml > ~/.terra/config/app.toml
-sed -i 's/enable-unsafe-cors = false/enable-unsafe-cors = '$ENABLE_UNSAFE_CORS'/g' ~/.terra/config/app.toml
-sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = '$ENABLE_UNSAFE_CORS'/g' ~/.terra/config/app.toml
+rm ~/.terra/config/app.toml
+rm ~/.terra/config/config.toml
 
-# Needed to use awk to replace this multiline string.
-if [ "$ENABLE_LCD" = true ] ; then
-  gawk -i inplace '/^# Enable defines if the API server should be enabled./,/^enable = false/{if (/^enable = false/) print "# Enable defines if the API server should be enabled.\nenable = true"; next} 1' ~/.terra/config/app.toml
-fi
+cp ~/app.toml ~/.terra/config/app.toml
+cp ~/config.toml ~/.terra/config/config.toml
 
-# config.toml updates
+toml set --toml-path /terra/.terra/config/app.toml minimum-gas-prices $MINIMUM_GAS_PRICES
+toml set --toml-path /terra/.terra/config/app.toml enable-unsafe-cors $ENABLE_UNSAFE_CORS
+toml set --toml-path /terra/.terra/config/app.toml enabled-unsafe-cors $ENABLE_UNSAFE_CORS
+toml set --toml-path /terra/.terra/config/app.toml api.enable $ENABLE_LCD
 
-sed 's/moniker = "moniker"/moniker = "'"$MONIKER"'"/g' ~/config.toml > ~/.terra/config/config.toml
-sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' ~/.terra/config/config.toml
-sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = '"$CORS_ALLOWED_ORIGINS"'/g' ~/.terra/config/config.toml
-sed -i 's/cors_allowed_methods = \["HEAD", "GET", "POST", \]/cors_allowed_methods = '"$CORS_ALLOWED_METHODS"'/g' ~/.terra/config/config.toml
-sed -i 's/cors_allowed_headers = \["Origin", "Accept", "Content-Type", "X-Requested-With", "X-Server-Time", \]/cors_allowed_headers = '"$CORS_ALLOWED_HEADERS"'/g' ~/.terra/config/config.toml
+toml set --toml-path /terra/.terra/config/config.toml moniker $MONIKER
+toml set --toml-path /terra/.terra/config/config.toml rpc.laddr 0.0.0.0:26657
+toml set --toml-path /terra/.terra/config/config.toml cors_allowed_origins $CORS_ALLOWED_ORIGINS
+toml set --toml-path /terra/.terra/config/config.toml cors_allowed_methods $CORS_ALLOWED_METHODS
+toml set --toml-path /terra/.terra/config/config.toml cors_allowed_headers $CORS_ALLOWED_HEADERS
 
-if [ "$CHAINID" = "columbus-5" ] && [[ ! -z "$SNAPSHOT_NAME" ]] ; then 
+if [ "$CHAINID" = "columbus-5" ] && [ ! -z "$SNAPSHOT_NAME" ] ; then 
   # Download the snapshot if data directory is empty.
   res=$(find "$DATADIR" -name "*.db")
   if [ "$res" ]; then
@@ -52,7 +50,7 @@ if [ "$CHAINID" = "columbus-5" ] && [[ ! -z "$SNAPSHOT_NAME" ]] ; then
   fi
 fi
 
-terrad start $TERRAD_STARTUP_PARAMETERS &
+terrad start --x-crisis-skip-assert-invariants &
 
 #Wait for Terrad to catch up
 while true
