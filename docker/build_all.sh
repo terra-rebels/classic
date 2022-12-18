c3bc767f59a9653a6063716bb67bc9c9fbcdc167
echel0n-tr <echel0n@terrarebels.net>
2022-12-18 12:28:00 -0800

    updated docker files

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-VERSION="${1:-a8bc017fcb10cf0cc55e4b0036e7a1bf7ef0ad1b}"
+VERSION="${1:-a6f1a39f00c2723b62f42d40d024bd1181225a8d}"
 
-pushd .. 
-
-git checkout $VERSION
-docker build -t terrarebels/terraclassic.terrad-binary:$VERSION .
-git checkout -
-
-popd
-
+docker build --build-arg version=$VERSION -t terrarebels/terraclassic.terrad-binary:$VERSION . -f ./Dockerfile.terraclassic.terrad-binary
 docker build --build-arg version=$VERSION --build-arg chainid=columbus-5 -t terrarebels/terraclassic.terrad-node:$VERSION-columbus-5 .
-docker build --build-arg version=$VERSION --build-arg chainid=rebel-1    -t terrarebels/terraclassic.terrad-node:$VERSION-rebel-1    .
-docker build --build-arg version=$VERSION --build-arg chainid=rebel-2    -t terrarebels/terraclassic.terrad-node:$VERSION-rebel-2    .
+docker build --build-arg version=$VERSION --build-arg chainid=rebel-1    -t terrarebels/terraclassic.terrad-node:$VERSION-rebel-1 .
+docker build --build-arg version=$VERSION --build-arg chainid=rebel-2    -t terrarebels/terraclassic.terrad-node:$VERSION-rebel-2 .
 
